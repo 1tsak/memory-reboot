7f3eb8a97def84cc38aa48ce6018f2394a8194aa
A2v10 <hey.a2v10@gmail.com>
2024-04-07 13:58:55 +0530

    Added Tabs & Fixed Layout (Auth)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,39 +11,82 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <Card className="md:w-1/3">
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl">Login to your account</CardTitle>
-          <CardDescription>
-            Enter your email below to login
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground"></span>
-            </div>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">Login</Button>
-        </CardFooter>
-      </Card>
+      <Tabs defaultValue="student" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="student">I'm a Student</TabsTrigger>
+          <TabsTrigger value="teacher">I'm a Teacher</TabsTrigger>
+        </TabsList>
+        <TabsContent value="student">
+          <Card>
+            <CardHeader className="space-y-1">
+              <CardTitle className="text-2xl">Login to your account</CardTitle>
+              <CardDescription>Enter your email below to login</CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground"></span>
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">Login</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="teacher">
+          <Card>
+            <CardHeader className="space-y-1">
+              <CardTitle className="text-2xl">Login to your account</CardTitle>
+              <CardDescription>Enter your email below to login</CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground"></span>
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">Login</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+      <p className="m-2">
+        Don't Have an account?{" "}
+        <Link href="/signup">
+          <span className="text-blue-500 font-semibold">Register</span>
+        </Link>
+      </p>
     </div>
   );
 }
