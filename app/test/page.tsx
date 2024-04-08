6bbebaf75524347679ed5a816acf72c6eab8e705
Akarshit7 <akarshitbatra7@gmail.com>
2024-04-08 12:11:17 +0530

    test page

---
+++ app/test/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen fixed w-screen' style={{ 
+        backgroundImage: `url("/mcq_bg.png")` 
+      }}>
+        <nav className='h-[100px] mt-10 rounded-md bg-white mb-10 mx-8'>
+
+        </nav>
+        <div className='flex flex-row m-10 space-x-4 h-[70%]'>
+            <div className='bg-white h-full w-[25%] rounded-lg'>
+fsdfs
+            </div>
+            <div className='bg-white flex-grow rounded-lg'>
+                fsdaf
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default page
